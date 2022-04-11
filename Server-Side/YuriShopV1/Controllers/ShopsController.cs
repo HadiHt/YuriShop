08af629542d82a1a32a3c99a3ca411e7441bb720
HadiHt <70974962+HadiHt@users.noreply.github.com>
2022-04-11 16:06:20 +0300

    Added Shop Controller Basic Endpoints/User address and card Endpoints

--- Server-Side/YuriShopV1/Controllers/ShopsController.cs
+++ Server-Side/YuriShopV1/Controllers/ShopsController.cs
@@ -1,6 +1,51 @@
-﻿namespace YuriShopV1.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using YuriShopV1.Data.Users;
+using YuriShopV1.Models;
+
+namespace YuriShopV1.Controllers
 {
-    public class ShopsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopsController : ControllerBase
     {
+        private readonly IShopRepo _shopRepo;
+        private readonly IAddressRepo _addressRepo;
+        private readonly ICardRepo _cardRepo;
+        private readonly IUserRepo _userRepo;
+
+
+        public ShopsController(IShopRepo shopRepo ,IAddressRepo addressRepo, ICardRepo cardRepo, IUserRepo userRepo)
+        {
+            _shopRepo = shopRepo;
+            _addressRepo = addressRepo;
+            _cardRepo = cardRepo;
+            _userRepo = userRepo;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Shop>> GetAllShops()
+        {
+            return Ok(_shopRepo.GetAllShops());
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Shop> GetShopById(int id)
+        {
+            return Ok(_shopRepo.GetShopById(id));
+        }
+
+        [HttpGet("{id}/address")]
+        public ActionResult<Address> GetAddressByShopId(int id)
+        {
+            var address = _addressRepo.GetAddressByShopId(id);
+            return Ok(address);
+        }
+
+        [HttpGet("{id}/card")]
+        public ActionResult<Card> GetCardByShopId(int id)
+        {
+            var card = _cardRepo.GetCardByShopId(id);
+            return Ok(card);
+        }
     }
 }
