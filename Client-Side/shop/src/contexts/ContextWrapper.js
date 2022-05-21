d09bf8736e78db58396c607723c412f700393a61
Mohamad Khoder <Mohamad_Khoder@outlook.com>
2022-05-22 00:30:48 +0300

    test

--- Client-Side/shop/src/contexts/ContextWrapper.js
+++ Client-Side/shop/src/contexts/ContextWrapper.js
@@ -3,19 +3,15 @@ import { UserProvider } from "./userContext";
 import { CartProvider } from "./cartContext";
 import { CardProvider } from "./cardContext";
 import { AddressProvider } from "./addressContext";
-<<<<<<< HEAD
 import { OrderProvider } from "./orderListContext";
 import { ShopProvider } from "./shopContext";
 import { ImageProvider } from "./imageContext";
 import { AllProductsProvider } from "./allProductsContext";
-=======
->>>>>>> d8d94adfeb3748a60ccbeb26c24810953efe4451
 
 const ContextWrapper = (props) => {
     
     return (
         <UserProvider>
-<<<<<<< HEAD
             <ShopProvider>
                 <CartProvider>
                     <AddressProvider>
@@ -31,15 +27,6 @@ const ContextWrapper = (props) => {
                     </AddressProvider>
                 </CartProvider>
             </ShopProvider>
-=======
-            <CartProvider>
-                <AddressProvider>
-                    <CardProvider>
-                        {props.children}
-                    </CardProvider>
-                </AddressProvider>
-            </CartProvider>
->>>>>>> d8d94adfeb3748a60ccbeb26c24810953efe4451
         </UserProvider>
     );
 }
