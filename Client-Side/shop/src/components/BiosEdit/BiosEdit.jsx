2840a0fd701816d29f506c3ce119cc3b773629a5
HadiHt <70974962+HadiHt@users.noreply.github.com>
2022-05-25 20:03:43 +0300

    Fixed EditBios

--- Client-Side/shop/src/components/BiosEdit/BiosEdit.jsx
+++ Client-Side/shop/src/components/BiosEdit/BiosEdit.jsx
@@ -3,19 +3,38 @@ import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { userContext } from "../../contexts/userContext";
 import { useContext } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 import "./BiosEdit.css";
 
 const BiosEdit = (props) => {
   const { user, setUser } = useContext(userContext);
+  const [tempUser, SetTempUser] = useState(user);
   let navigate = useNavigate();
+  const params = useParams();
 
+  useEffect(() => {
+    if (user.userId !== params.id) {
+      Axios.get("http://localhost:5000/api/users/" + params.id)
+        .then((res) => {
+          SetTempUser((prevUser) => (prevUser = res.data));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [params.id]);
   const routeChange = () => {
-    let path = `/UserProfile`;
+    let path = `/UserProfile/` + params.id;
     navigate(path);
   };
 
   const updateUser = () => {
-    Axios.put("http://localhost:5000/api/Users/" + user.userId + "/user", user)
+    Axios.put(
+      "http://localhost:5000/api/Users/" + params.id + "/user",
+      tempUser
+    )
       .then((res) => {
         console.log(res.data);
         routeChange();
@@ -30,28 +49,28 @@ const BiosEdit = (props) => {
         <div className="FirstEditFlex">
           <p>First Name:</p>
           <input
-            onChange={(e) => (user.firstName = e.target.value)}
+            onChange={(e) => (tempUser.firstName = e.target.value)}
             className="EditBiosInput"
           ></input>
         </div>
         <div className="SecondEditFlex">
           <p>Last Name:</p>
           <input
-            onChange={(e) => (user.lastName = e.target.value)}
+            onChange={(e) => (tempUser.lastName = e.target.value)}
             className="EditBiosInput"
           ></input>
         </div>
         <div className="ThirdEditFlex">
           <p>Username:</p>
           <input
-            onChange={(e) => (user.username = e.target.value)}
+            onChange={(e) => (tempUser.username = e.target.value)}
             className="EditBiosInput"
           ></input>
         </div>
         <div className="ForthEditFlex">
           <p>Phone Number:</p>
           <input
-            onChange={(e) => (user.phoneNumber = e.target.value)}
+            onChange={(e) => (tempUser.phoneNumber = e.target.value)}
             className="EditBiosInput"
           ></input>
         </div>
